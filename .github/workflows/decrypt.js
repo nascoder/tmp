16344f1ba186c3f60e0edd1194ffffcefc62ba57
ntohidi <nasrin@kidocode.com>
2019-12-16 14:47:53 +0800

    update

--- .github/workflows/decrypt.js
+++ .github/workflows/decrypt.js
@@ -1,17 +1,17 @@
 const crypto = require('crypto');
 // const axios = require("axios");
 
-async function decrypt(content, pass) {
+async function decrypt(repo, pass) {
     console.log("Decrypting content...");
 
     const algorithm = 'aes256';
 
     try {
-        // let resp = axios.get(
-        //     `https://api.github.com/repos/${repo}/contents/auth.enc`
-        // )
-        // let cnt = resp.data.content
-        // let content = Buffer.from(cnt, 'base64').toString('ascii').replace(/\n/g, "");
+        let resp = axios.get(
+            `https://api.github.com/repos/${repo}/contents/auth.enc`
+        )
+        let cnt = resp.data.content
+        let content = Buffer.from(cnt, 'base64').toString('ascii').replace(/\n/g, "");
 
         console.log(content)
         var decipher = crypto.createDecipher(algorithm, pass)
@@ -24,8 +24,8 @@ async function decrypt(content, pass) {
     }
 }
 
-const a = async (content, gitToken) => {
-    return await decrypt(content, gitToken)
+const a = async (repo, gitToken) => {
+    return await decrypt(repo, gitToken)
 }
 
 a(process.argv[2], process.argv[3]).then((res) => {
