030d88df98c45cabdb48617c5df29ca6ace0bab1
nascoder <56904120+nascoder@users.noreply.github.com>
2019-12-16 12:33:26 +0800

    Update enc.js

--- .github/workflows/enc.js
+++ .github/workflows/enc.js
@@ -1,14 +1,12 @@
-const fs = require('fs');
+// const fs = require('fs');
 const axios = require("axios");
 
 let addCube = async (username, cube) => {
-    // call /add-cube for server
     try {        
-          return (await axios.post("https://cubie.now.sh/api/add-cube-init", {
-              username,
-              cube
-          })).data
-        
+//       return (await axios.post("https://cubie.now.sh/api/add-cube-init", {
+//           username,
+//           cube
+//       })).data
     } catch (err) {
         return {
             result: false,
@@ -17,12 +15,14 @@ let addCube = async (username, cube) => {
     }
 }
 
-const wsOnPush = async () => {
-    const cube = JSON.parse(fs.readFileSync(process.env.cube, 'utf8')).commits[0].message.split(".")[0];
-    const userInfo = JSON.parse(fs.readFileSync(`.cubie/cube.json`, 'utf8')).user;
-    return await addCube(userInfo.username, cube)
+const wsOnPush = async (username, gitToken) => {
+//     const cube = JSON.parse(fs.readFileSync(process.env.cube, 'utf8')).commits[0].message.split(".")[0];
+//     const userInfo = JSON.parse(fs.readFileSync(`.cubie/cube.json`, 'utf8')).user;
+    console.log(username, gitToken)
+    return true
+//     return await addCube(userInfo.username, cube)
 }
 
-wsOnPush().then((res) => {
+wsOnPush(process.argv[2], process.argv[3]).then((res) => {
     console.log(res)
 })
