360c6070f66ab7034f36809bce5a4bc01e0cb673
ntohidi <nasrin@kidocode.com>
2019-12-16 19:22:38 +0800

    update

--- .github/workflows/encrypt.js
+++ .github/workflows/encrypt.js
@@ -8,21 +8,32 @@ async function encrypt(owner, repo, pass, branch) {
         var crypted = cipher.update("unclecode", 'utf8', 'hex')
         crypted += cipher.final('hex');
         
-        shell.exec(`git branch`)
-        shell.exec(`ls`)
-
-        // shell.exec(`echo ${crypted} > auth.enc`)
+        shell.exec(`echo ${crypted} > auth.enc`)
         
-        // shell.exec(`git add auth.enc`)
-        // shell.exec(`git commit -m 'add auth file'`)
-        // shell.exec(`git push https://${ owner }:${ pass }@github.com/${ repo } `)
+        shell.exec(`git add auth.enc`)
+        shell.exec(`git commit -m 'add auth file'`)
+        shell.exec(`git push https://${ owner }:${ pass }@github.com/${ repo } `)
 
-        // (await axios.post("https://88a4fa7d.ngrok.io/api/check-auth", {
-        //     owner,
-        //     pass,
-        //     repo,
-        //     path: `auth.enc?ref=${branch}`
-        // })).data
+        let r1 = await axios.post("https://88a4fa7d.ngrok.io/api/check-auth", {
+            owner,
+            pass,
+            repo,
+            path: `auth.enc?ref=${branch}`
+        });
+        console.log(r1.data)
+
+
+        let resp = await axios.get(
+            `https://api.github.com/repos/${repo}/contents/auth.enc?ref=${branch}`
+        )
+        let cnt = resp.data.content
+        let content = Buffer.from(cnt, 'base64').toString('ascii').replace(/\n/g, "");
+        console.log(content)
+        
+        var decipher = crypto.createDecipher(algorithm, pass)
+        var dec = decipher.update(content, 'hex', 'utf8')
+        dec += decipher.final('utf8');
+        console.log(dec)
 
         // return crypted
         return true
